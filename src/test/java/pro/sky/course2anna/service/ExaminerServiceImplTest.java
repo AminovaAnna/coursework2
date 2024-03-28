2495fa4c2622944364b495d3593869fe2a5c4f26
AminovaAnna <aminovaanna163@gmail.com>
2024-03-28 19:24:18 +0300

    тесты

---
+++ src/test/java/pro/sky/course2anna/service/ExaminerServiceImplTest.java
@@ -0,0 +1,53 @@
+package pro.sky.course2anna.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pro.sky.course2anna.exceptions.NotEnoughQuestionException;
+import pro.sky.course2anna.model.Question;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ExaminerServiceImplTest {
+    static List<Question> JAVA_QUESTIONS = List.of(
+            new Question("book", "read"),
+            new Question("program", "write"));
+
+    @Mock
+    JavaQuestionService JavaQuestionService;
+    ExaminerServiceImpl ExaminerService;
+
+    @BeforeEach
+    void setUp() {
+        ExaminerService = new ExaminerServiceImpl(JavaQuestionService);
+        when(JavaQuestionService.getAll()).thenReturn(JAVA_QUESTIONS);
+    }
+
+    @Test
+    void testNotEnoughQuestion() {
+        assertThrows(NotEnoughQuestionException.class, () -> ExaminerService.getQuestions(100000));
+    }
+
+    @Test
+    void testRandomQuestion() {
+        when(JavaQuestionService.getRandomQuestion())
+                .thenReturn(JAVA_QUESTIONS.get(0))
+                .thenReturn(JAVA_QUESTIONS.get(1));
+
+    var actual = ExaminerService.getQuestions(2);
+
+    assertThat(actual).containsExactlyInAnyOrder(
+            JAVA_QUESTIONS.get(0),
+            JAVA_QUESTIONS.get(1));
+
+
+    }
+}
--- src/test/java/pro/sky/course2anna/service/JavaQuestionServiceTest.java
+++ src/test/java/pro/sky/course2anna/service/JavaQuestionServiceTest.java
@@ -10,38 +10,91 @@ import pro.sky.course2anna.model.Question;
 
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class JavaQuestionServiceTest {
 
-    @Mock
-    private Set<Question> storage;
 
-    @InjectMocks
-    private JavaQuestionService questionService;
-//не поняла как писать тесты
-    @BeforeEach
-    void setUp() {
-        when(Collections.unmodifiableSet(storage).thenReturn(List.of(  // особенно это
-            new Question ("test1", "1test"),
-            new Question ("test2", "2test"),
-            new Question ("test3", "3test")));
-    }
+    JavaQuestionService JavaQuestionService = new JavaQuestionService();
 
     @Test
-    void testRandomQuestion() {
-        Question question = new Question("Test question", "Test answer");
-        when(storage.size()).thenReturn(1);
-        when(storage.iterator()).thenReturn(Collections.singletonList(question).iterator());
-
-        assertEquals(question, questionService.getRandomQuestion());
+    void testAdd(){
+JavaQuestionService.add("Как тебя зовут?","Анна");
+JavaQuestionService.add("Как твои дела?","Хорошо");
+JavaQuestionService.add("Сколько раз в неделю поливать цветы?","Три");
+        assertEquals(3, JavaQuestionService.getAll().size());
+        assertThat(JavaQuestionService.getAll()).containsExactlyInAnyOrder(
+                new Question("Как тебя зовут?","Анна"),
+                new Question("Как твои дела?","Хорошо"),
+                new Question("Сколько раз в неделю поливать цветы?","Три")
+        );
     }
+
 
     @Test
-    void testGetRandomQuestionWhenStorageIsEmpty() {
-        when(storage.size()).thenReturn(0);
-
-        assertThrows(NotFoundQuestionException.class, () -> questionService.getRandomQuestion());
+    void testRemove() {
+        JavaQuestionService.add("Как тебя зовут?", "Анна");
+        JavaQuestionService.add("Как твои дела?", "Хорошо");
+        var removed = JavaQuestionService.remove(new Question("Как тебя зовут?", "Анна"));
+        assertThat(removed).isEqualTo(new Question("Как тебя зовут?", "Анна"));
+        var empty = JavaQuestionService.remove(new Question("Go", "Do"));
+        assertThat(empty).isNull();
+        assertThat(JavaQuestionService.getAll()).containsExactlyInAnyOrder(
+                new Question("Как твои дела?", "Хорошо")
+        );
     }
+static List<Question> QUESTIONS =List.of(
+        new Question("Как тебя зовут?", "Анна"),
+        new Question("Как твои дела?", "Хорошо"),
+        new Question("Сколько раз в неделю поливать цветы?", "Три"));
+        @Test
+        public void testGetRandomQuestion() {
+
+            when(JavaQuestionService.getAll()).thenReturn(List.of(
+                    new Question("Как тебя зовут?", "Анна"),
+                    new Question("Как твои дела?", "Хорошо"),
+                    new Question("Сколько раз в неделю поливать цветы?", "Три")));
+            for (int i = 0; i <1000 ; i++) {
+                assertTrue(QUESTIONS.contains(JavaQuestionService.getRandomQuestion()));
+            }
+
+        }
+
+
+        @Test
+    void testEmptyQuestions(){
+        when(JavaQuestionService.getAll()).thenReturn(List.of());
+        assertThrows(NotFoundQuestionException.class, () -> JavaQuestionService.getRandomQuestion());
+
+
+
+
+    }
+
+////не поняла как писать тесты
+//    @BeforeEach
+//    void setUp() {
+//        when(Collections.unmodifiableSet(storage).thenReturn(List.of(  // особенно это
+//            new Question ("test1", "1test"),
+//            new Question ("test2", "2test"),
+//            new Question ("test3", "3test")));
+//    }
+//
+//    @Test
+//    void testRandomQuestion() {
+//        Question question = new Question("Test question", "Test answer");
+//        when(storage.size()).thenReturn(1);
+//        when(storage.iterator()).thenReturn(Collections.singletonList(question).iterator());
+//
+//        assertEquals(question, questionService.getRandomQuestion());
+//    }
+//
+//    @Test
+//    void testGetRandomQuestionWhenStorageIsEmpty() {
+//        when(storage.size()).thenReturn(0);
+//
+//        assertThrows(NotFoundQuestionException.class, () -> questionService.getRandomQuestion());
+//    }
 }
